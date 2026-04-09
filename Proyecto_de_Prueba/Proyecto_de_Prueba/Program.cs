69ccd28dd9f03b502eae2365a68be239f634dddb
jorgenick <65787321+jorgenick@users.noreply.github.com>
2026-04-09 20:55:01 -0300

    comit de proyecto de prueba

---
+++ Proyecto_de_Prueba/Proyecto_de_Prueba/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Proyecto_de_Prueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("este es mi primer proyecto de prueba");
+        }
+    }
+}
