f916c873bdb69386384890eeda43e68ba32602a9
jorgenick <65787321+jorgenick@users.noreply.github.com>
2026-04-09 20:57:24 -0300

    2 prueba

--- Proyecto_de_Prueba/Proyecto_de_Prueba/Program.cs
+++ Proyecto_de_Prueba/Proyecto_de_Prueba/Program.cs
@@ -11,6 +11,7 @@ namespace Proyecto_de_Prueba
         {
 
             Console.WriteLine("este es mi primer proyecto de prueba");
+            Console.WriteLine("estamos probando el puente con al maquina virtual");
         }
     }
 }
