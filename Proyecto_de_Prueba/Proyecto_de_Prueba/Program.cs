35b49180442a87bc2c9940b1b27a4947a2a96d9f
jorgenick <65787321+jorgenick@users.noreply.github.com>
2026-04-10 11:12:54 -0300

    metodo Sumar
    prueba de consola

--- Proyecto_de_Prueba/Proyecto_de_Prueba/Program.cs
+++ Proyecto_de_Prueba/Proyecto_de_Prueba/Program.cs
@@ -7,12 +7,37 @@ namespace Proyecto_de_Prueba
 {
     class Program
     {
+
+
+
         static void Main(string[] args)
         {
 
             Console.WriteLine("este es mi primer proyecto de prueba");
             Console.WriteLine("estamos probando el puente con al maquina virtual");
             Console.WriteLine("probando la maquina virtual");
+
+            Console.WriteLine("ingrese 1er numero");
+
+            String numero1 = Console.ReadLine();
+            int numero1i = int.Parse(numero1);
+
+            Console.WriteLine("ingrese 2do numero");
+
+            String numero2 = Console.ReadLine();
+            int numero2i = int.Parse(numero2);
+
+            Console.WriteLine("LA SUMA ES IGUAL A " + SumarNumeros(numero1i, numero2i));
+            Console.ReadKey();
         }
+
+        public static int SumarNumeros(int numero1, int numero2)
+        {
+            int resultado = numero1 + numero2;
+
+            return resultado;
+
+        }
+
     }
 }
