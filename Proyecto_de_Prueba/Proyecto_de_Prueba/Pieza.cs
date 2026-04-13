ff02745c57005b7bc8dd98171ec1713ecdce2c1d
unknown <65787321+jorgenick@users.noreply.github.com>
2026-04-13 17:41:25 -0300

    comit 13 4 26

---
+++ Proyecto_de_Prueba/Proyecto_de_Prueba/Pieza.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Proyecto_de_Prueba
+{
+    class Pieza
+    {
+
+        private string nombre;
+        private double largo;
+
+
+        public double ConvertirCM(double numero) {
+            Console.WriteLine("se convierte el numero en centimetros");
+            return 0;
+        
+        }
+
+
+
+        public void EmitirMensaje() {
+            Console.WriteLine("MENSAJE EMITIDO");
+        }
+
+    }
+}
