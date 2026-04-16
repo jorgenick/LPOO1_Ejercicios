858f27993820445dc0fb8c55f1d8acadec1fb425
unknown <65787321+jorgenick@users.noreply.github.com>
2026-04-16 16:52:10 -0300

    practica teoria 2

--- Proyecto_de_Prueba/Ejercicios_Clase_2/Program.cs
+++ Proyecto_de_Prueba/Ejercicios_Clase_2/Program.cs
@@ -9,49 +9,111 @@ namespace Ejercicios_Clase_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ingrese el primer numero entero");
-            //String numero1String = ;
+            //Console.WriteLine("ingrese el primer numero entero");
+            ////String numero1String = ;
+            //int numero1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("NUMERO INGRESADO = " + numero1);
+
+            //Console.WriteLine("ingrese el segundo numero entero");
+            ////String numero2String = ;
+            //int numero2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("NUMERO INGRESADO = " + numero2);
+
+            //if (numero1 > numero2)
+            //{
+
+            //    Console.WriteLine("el numero " + numero1 + " es mayor que " + numero2);
+
+            //}
+            //else if (numero2 > numero1)
+            //{
+            //    Console.WriteLine("el numero " + numero2 + " es mayor que " + numero1);
+
+            //}
+            //else
+            //{
+
+            //    Console.WriteLine("LOS NUMEROS SON IGUALES");
+            //}
+
+
+
+            //if (numero1 % numero2 == 0)
+            //{
+            //    Console.WriteLine("el primer numero es multiplo del segundo numero");
+
+            //}
+            //else
+            //{
+
+            //    Console.WriteLine("el primer numero no es multiplo del segundo numero");
+            //}
+
+
+            //Console.ReadLine();
+
+
+            Herramientas h = new Herramientas();
+            h.Ingreso_de_Numeros();
+
+
+        }
+    }
+
+    class Herramientas
+    {
+
+        public void Ingreso_de_Numeros()
+        {
+            Console.WriteLine("ingrese el primer numero");
             int numero1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("NUMERO INGRESADO = " + numero1);
 
-            Console.WriteLine("ingrese el segundo numero entero");
-            //String numero2String = ;
+            Console.WriteLine("Ingrese el segundo numero");
             int numero2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("NUMERO INGRESADO = " + numero2);
 
+            Console.WriteLine("numeros ingresados: \n" + "Numero 1 : " + numero1 + "\nNumero 2 :" + numero2);
+            Console.ReadLine();
+
+            int numeroMayor = CompararNumeros(numero1, numero2);
+            DeterminarMultiplos(numero1, numero2);
+            Console.ReadLine();
+        }
+
+        public int CompararNumeros(int numero1, int numero2)
+        {
+            int numeroMayor;
             if (numero1 > numero2)
             {
-
-                Console.WriteLine("el numero " + numero1 + " es mayor que " + numero2);
-
+                Console.WriteLine("el primer numero ingresado es mayor que el segundo numero");
+                numeroMayor = numero1;
             }
-            else if (numero2 > numero1)
+            else if (numero1 < numero2)
             {
-                Console.WriteLine("el numero " + numero2 + " es mayor que " + numero1);
-
+                Console.WriteLine("el segundo numero ingresado en mayor que el primer numero");
+                numeroMayor = numero2;
             }
             else
             {
-
-                Console.WriteLine("LOS NUMEROS SON IGUALES");
+                Console.WriteLine("los numeros ingresados son iguales");
+                numeroMayor = 0;
             }
 
+            return numeroMayor;
+        }
 
-
+        public void DeterminarMultiplos(int numero1, int numero2)
+        {
             if (numero1 % numero2 == 0)
             {
-                Console.WriteLine("el primer numero es multiplo del segundo numero");
-
+                Console.WriteLine("el primer numero ES multiplo del segundo numero");
             }
-            else {
-
-                Console.WriteLine("el primer numero no es multiplo del segundo numero");
+            else
+            {
+                Console.WriteLine("el primer numero NO es multiplo del segundo");
             }
-
-
-            Console.ReadLine();
 
         }
+
     }
 
 }
